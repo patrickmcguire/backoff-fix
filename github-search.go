9fda15293b93127dad3b5dd621585c879bcbb46e
Patrick McGuire <pjm1988@gmail.com>
2022-08-20 14:26:55 -0400

    Add some backoff

--- github-search.go
+++ github-search.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"strings"
-	"github.com/google/go-github/github"
 	"context"
+	"fmt"
+	"github.com/google/go-github/github"
+	"math"
+	"strings"
 	"time"
 )
 
@@ -14,18 +16,29 @@ func fetchMatches(
 
 	opts := &github.SearchOptions{TextMatch: true, ListOptions: github.ListOptions{Page: 0, PerPage: 100}}
 	for {
-		result, response, err := client.Search.Code(context.Background(), search, opts)
-		if err != nil {
-			return nil, err
+		result, response, firstErr := client.Search.Code(context.Background(), search, opts)
+		if firstErr != nil {
+			retryCount := 1
+			for retryCount < 15 {
+				backoff(retryCount)
+				result, response, err = client.Search.Code(context.Background(), search, opts)
+				if err == nil {
+					break
+				}
+			}
+
+			return results, firstErr
 		}
 
-		for _, codeResult := range(result.CodeResults) {
+		for _, codeResult := range result.CodeResults {
 			results = append(results, codeResult)
 		}
 
 		if response.NextPage == 0 {
 			break
 		}
+
+		fmt.Println("Page", opts.ListOptions.Page, "completed")
 		opts.ListOptions.Page = response.NextPage
 		time.Sleep(5000 * time.Millisecond)
 	}
@@ -36,21 +49,27 @@ func fetchMatches(
 func selectExactMatches(results []github.CodeResult, exactSearch string) []github.CodeResult {
 	// this doesn't currently handle pagination, but the current result set is one page
 	var exactResults []github.CodeResult
-	for _, result := range(results) {
+	for _, result := range results {
 		matches := result.TextMatches
 		shouldAdd := false
-		for _, match := range(matches) {
-			if (strings.Contains(*match.Fragment, exactSearch)) {
+		for _, match := range matches {
+			if strings.Contains(*match.Fragment, exactSearch) {
 				shouldAdd = true
 			}
 		}
 
-		if (shouldAdd) {
+		if shouldAdd {
 			exactResults = append(exactResults, result)
 		}
 	}
 
 	return exactResults
+}
+
+func backoff(attemptNumber int) {
+	sleepTime := math.Pow(1.5, (float64(attemptNumber))) * 5000
+	rounded := math.Round(sleepTime)
+	time.Sleep(time.Duration(rounded))
 }
 
 func SearchGithub(
@@ -66,4 +85,3 @@ func SearchGithub(
 	exactResults = selectExactMatches(results, exactSearch)
 	return exactResults, nil
 }
-
