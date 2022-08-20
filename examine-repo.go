340f1fde43534134ab9ede5be187d05fc7d20715
Patrick McGuire <pjm1988@gmail.com>
2022-08-20 13:25:19 -0400

    Fix typo

--- examine-repo.go
+++ examine-repo.go
@@ -7,8 +7,8 @@ import (
 )
 
 func ExamineRepository(location string) (err error) {
-	fset := token.NewFileSet()
-	parsedDir, err := parser.ParseDir(fset, location, nil, 0)
+	fSet := token.NewFileSet()
+	parsedDir, err := parser.ParseDir(fSet, location, nil, 0)
 	if err != nil {
 		fmt.Println(err)
 		return err
